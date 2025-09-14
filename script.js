489b635ee9c82bba98562a1232a7bc0373d72304
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-09-14 17:52:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function changeColor() {
+	   const inputId = document.getElementById("input-id").value;
+     const color = document.getElementById("color-id").value;
+   
+     const cells = document.querySelectorAll(".cell");
+	for(let cell of cells){
+		 cell.style.backgroundColor = "transparent";
+	}
+	const choosenCell =document.getElementById(inputId);
+	if(choosenCell){
+		choosenCell.style.backgroundColor = color;
+	}else{
+         alert("Invalid cell id! Please enter a number between 1 and 9.");
+    }
+}
